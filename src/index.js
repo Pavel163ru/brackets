f0edfa0cf422a1027aca6bf5fb499817f928f2f1
Pavel R <work202006@gmail.com>
2021-02-20 15:10:23 +0400

    feat: make it work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var arr = str.split('');
+  var stack = [];
+  var isOpen = false;
+  for(var i = 0; i < arr.length; i++){
+    if(isOpen){
+      var close = bracketsConfig.find(b => b[1] == arr[i] && stack[stack.length - 1] == b[0]);
+      if(close){
+        stack.pop();
+        continue;
+      }
+    }
+    var open = bracketsConfig.find(b => b[0] == arr[i]);
+    if(open){
+      isOpen = true;
+      stack.push(arr[i]);
+    }else{
+      var close = bracketsConfig.find(b => b[1] == arr[i] && stack[stack.length - 1] == b[0]);
+      if(close){
+        stack.pop();
+      }else{
+        return false;
+      }
+    }
+
+  }
+  if(stack.length == 0){
+    return true;
+  }else{
+    return false;
+  }
 }
